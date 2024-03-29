c1e9f1f385b5a8c6aabd02349f952fc5e4fde6d3
Ilya Pavlov <ilyapav@icloud.com>
2024-03-29 18:33:38 +0300

    Initial Commit

---
+++ VKServiceList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKServiceList
+//
+//  Created by ily.pavlov on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
